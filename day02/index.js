c5707e8cd5634b6cb08c5c0125afedd6599e2e17
Michael Bodell <mbodell@gmail.com>
2025-12-02 03:31:38 -0800

    Solved part 1 of day 2

--- day02/index.js
+++ day02/index.js
@@ -6,8 +6,18 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let ranges = [];
 
 eachLine(filename, function(line) {
+	ranges = line.split(',').map((x)=>x.split('-').map((y)=>parseInt(y)));
 }).then(function(err) {
+	for(let i=0;i<ranges.length;i++) {
+		for(let id=ranges[i][0];id<=ranges[i][1];id++) {
+			let idstr = id.toString();
+			if(idstr.length%2===0 && idstr.substr(0,idstr.length/2) === idstr.substr(idstr.length/2)) {
+				answer += id;
+			}
+		}
+	}
   console.log(answer);
 });
