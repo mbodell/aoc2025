b3912587637a4d18bbdc5031851e977e4070f235
Michael Bodell <mbodell@gmail.com>
2025-12-04 21:25:36 -0800

    Solved day 5 part 1

--- day05/index.js
+++ day05/index.js
@@ -6,8 +6,31 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let ranges =[];
+let type = 1;
+let ing = [];
 
 eachLine(filename, function(line) {
+	if(type===1) {
+		if(line.length===0) {
+			type++;
+		} else {
+			ranges.push(line.split('-').map((x)=>parseInt(x)));
+		}
+	} else {
+		ing.push(parseInt(line));
+	}
 }).then(function(err) {
+	for(let i=0;i<ing.length;i++) {
+		let fresh=0;
+		for(let j=0;j<ranges.length;j++) {
+			if(ing[i]>=ranges[j][0]&&ing[i]<=ranges[j][1]) {
+				fresh++;
+			}
+		}
+		if(fresh>0) {
+			answer++;
+		}
+	}
   console.log(answer);
 });
