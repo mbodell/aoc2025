89a25a8a6c810c04b6acfe1388425f78dd62afc1
Michael Bodell <mbodell@gmail.com>
2025-12-06 21:32:20 -0800

    Solved day 7 part 1

--- day07/index.js
+++ day07/index.js
@@ -6,8 +6,31 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let grid = [];
 
 eachLine(filename, function(line) {
+	grid.push(line.split(''));
 }).then(function(err) {
+	for(let y=0;y<grid.length-1;y++) {
+		for(let x=0;x<grid[y].length;x++) {
+			switch(grid[y][x]) {
+				case '.':
+				case '^':
+					break;
+				case 'S':
+					grid[y+1][x] = '|';
+					break;
+				case '|':
+					if(grid[y+1][x]==='.') {
+						grid[y+1][x] = '|';
+					} else if(grid[y+1][x]==='^') {
+						grid[y+1][x-1] = '|';
+						grid[y+1][x+1] = '|';
+					  answer++;
+					}
+					break;
+			}
+		}
+	}
   console.log(answer);
 });
