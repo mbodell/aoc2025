42c5e6ffbe0a27ce0952f0398dc8c35f90a596b3
Michael Bodell <mbodell@gmail.com>
2025-12-08 23:01:33 -0800

    Solved day 9 part 1

--- day09/index.js
+++ day09/index.js
@@ -6,8 +6,22 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let corners = [];
+
+function rectSize(a,b) {
+	return (Math.abs(a[0]-b[0])+1)*(Math.abs(a[1]-b[1])+1);
+}
 
 eachLine(filename, function(line) {
+	corners.push(line.split(",").map((x)=>parseInt(x)));
 }).then(function(err) {
+	for(let i=0;i<corners.length;i++) {
+		for(let j=i+1;j<corners.length;j++) {
+			let canSize = rectSize(corners[i],corners[j]);
+			if(canSize > answer) {
+				answer = canSize;
+			}
+		}
+	}
   console.log(answer);
 });
