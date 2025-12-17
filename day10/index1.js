4ce928fc1717ac9f00a9f73733760876f9c27d9c
Michael Bodell <mbodell@gmail.com>
2025-12-17 02:25:06 -0800

    Finally a good enough solution to part 2 By breaking the parity and divide by 2 approach with memoization we can solve the problem very, very, very fast

--- day10/index1.js
+++ day10/index1.js
@@ -8,128 +8,82 @@ let filename = process.argv.slice(2)[0] || 'input.txt';
 let answer = 0;
 let machines = [];
 
-function setLightN(l,n) {
-	return l.substr(0,n)+(l[n]==='.'?'#':'.')+l.substr(n+1);
-}
-
-function joltMatch(a,b) {
-	let ret = true;
-	if(a.length===b.length) {
-		for(let i=0;ret && i<a.length;i++) {
-			if(a[i]!==b[i]) {
-				ret = false;
-			}
-		}
-	} else {
-		ret = false;
-	}
-	//console.log(`Does ${a} match ${b}? ${ret}`);
-	return ret;
-}
-
-function findMaxPush(li,bu,jo) {
-	let ret = -1;
-	for(let i=0;i<bu.length;i++) {
-		let p = jo[bu[i]] - li[bu[i]];
-		if(ret===-1 || p<ret) {
-			ret = p;
-		}
-	}
-	return ret;
-}
-
-function pushButNTimes(cur,but,n) {
-	let ret = [];
-	for(let i=0;i<cur.length;i++) {
-		ret[i] = cur[i];
-	}
-	for(let i=0;i<but.length;i++) {
-		ret[but[i]] += n;
-	}
-	return ret;
-}
-function joltEqual(c,j) {
-	ret = true;
-	if(c.length !== j.length) {
-		ret = false;
-	}
-	for(let i=0;ret&&i<c.length;i++) {
-		if(c[i]!==j[i]) {
-			ret = false;
-		}
-	}
-	return ret;
-}
-function findDist(cur,jolt) {
-	let ret = 0;
-	for(let i=0;i<cur.length;i++) {
-		ret += jolt[i]-cur[i];
-	}
-	return ret;
-}
-
 let memo={};
 
-function solveMachine(cur,but,jolt,best) {
-	let str = ""+cur+":"+best+":"+but;
-	if(memo[str]) {
-		return memo[str];
+let tooBig = 0;
+let parityLights = [];
+
+function preCalc(but,jolt) {
+	memo={};
+	tooBig = jolt.reduce((a,b)=>a+b)+1;
+	let numLights = jolt.length;
+	parityLights = [];
+	for(let i=0;i<2**numLights;i++) {
+		parityLights[i] = [];
 	}
-	if(joltEqual(cur,jolt)) {
-		memo[str]=0;
-		return 0;
-	}
-	if(but.length===0) {
-		memo[str]=-1;
-		return -1;
-	}
-	let bestSolve = -1;
-	let maxPush = findMaxPush(cur,but[0],jolt);
-	if(best !== -1 && (best < maxPush)) {
-		memo[str]=-1;
-		return -1;
-	}
-	if(best !==-1 && (but[0].length*best<findDist(cur,jolt))) {
-		//console.log(`Cut out ${but[0].length} with ${best} and ${findDist(cur,jolt)}`);
-		memo[str]=-1;
-		return -1;
-	}
-	let butTail = [];
-	for(i=1;i<but.length;i++) {
-		butTail.push(but[i]);
-	}
-	//console.log(`Trying ${maxPush} button presses with ${but.length} buttons left trying ${cur} to get to ${jolt}`);
-	for(let push=maxPush;push>=0;push--) {
-		let work = pushButNTimes(cur,but[0],push);
-		let curBest = (bestSolve===-1)?-1:bestSolve-push;
-		let nextBest = solveMachine(work,butTail,jolt,curBest);
-		if(nextBest !== -1) {
-			console.log(`***** A solution with ${but.length} buttons with ${nextBest+push} as the solution (previous ${bestSolve})*****`);
-			if(bestSolve === -1 || (nextBest + push)<bestSolve) {
-				bestSolve = nextBest+push;
+	for(let i=0;i<2**but.length;i++) {
+		let toPush = i.toString(2);
+		while(toPush.length<but.length) {
+			toPush = "0" + toPush;
+		}
+		let lights = new Array(numLights).fill(0);
+		let tp = toPush.split("").map((x)=>parseInt(x));
+		let numTP = 0;
+		for(let j=0;j<tp.length;j++) {
+			if(tp[j]===1) {
+				numTP++;
+				for(let k=0;k<but[j].length;k++) {
+					lights[but[j][k]]++;
+				}
 			}
 		}
+		let idx = parseInt(lights.map((x)=>(x%2).toString()).reduce((a,b)=>a+b),2);
+		parityLights[idx].push([numTP,lights]);
 	}
-	memo[str]=bestSolve;
-	return bestSolve;
+}
+function solveMachine(but,jolt) {
+	let joltHash = jolt.reduce((a,b)=>""+a+","+b);
+	if(memo[joltHash]) {
+		return memo[joltHash];
+	}
+	let min = jolt.reduce((a,b)=>(a<b)?a:b);
+	let max = jolt.reduce((a,b)=>(a>b)?a:b);
+	if(min<0) {
+		memo[joltHash] = tooBig;
+		return tooBig;
+	}
+	if(max===0) {
+		memo[joltHash] = 0;
+		return 0;
+	}
+	let parity = parseInt(jolt.map((x)=>(x%2).toString()).reduce((a,b)=>a+b),2);
+	let ret = tooBig;
+	for(let i=0;i<parityLights[parity].length;i++) {
+		let can = parityLights[parity][i];
+		let canJolt = [];
+		for(let j=0;j<jolt.length;j++) {
+			canJolt[j]=(jolt[j]-can[1][j])/2;
+		}
+		let canAns = can[0] + 2 * solveMachine(but,canJolt);
+		if(canAns<ret) {
+			ret = canAns;
+		}
+	}
+
+	memo[joltHash] = ret;
+	return ret;
 }
 
 eachLine(filename, function(line) {
   let target = line.split("]")[0].split("[")[1];
 	let buttons = line.split("]")[1].split(" {")[0].split(")").map((x)=>(x.substr(x.indexOf("(")+1).split(',').map((y)=>parseInt(y))));
 	buttons.pop();
-	buttons.sort((a,b)=>b.length-a.length);
 	let joltage = line.split("{")[1].split("}")[0].split(",").map((x)=>parseInt(x));
 	machines.push([target,buttons,joltage]);
 }).then(function(err) {
 	for(let i=0;i<machines.length;i++) {
-		let minSol = -1;
-		memo={};
-		let lights = [];
-		for(let j=0;j<machines[i][0].length;j++) {
-			lights.push(0);
-		}
-		let solve = solveMachine(lights,machines[i][1],machines[i][2],-1);
+		preCalc(machines[i][1],machines[i][2]);
+		let solve = solveMachine(machines[i][1],machines[i][2]);
 		console.log(`solved machine ${i} with ${solve} button pushes`);
 		answer += solve;
 	}
