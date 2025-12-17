7957569f0ccf054f5c334aa587f85879bf5359c2
Michael Bodell <mbodell@gmail.com>
2025-12-17 03:29:26 -0800

    A divide and conquer memoization recursive solution for part 2 (still running)

--- day11/index1.js
+++ day11/index1.js
@@ -10,6 +10,26 @@ let answer = 0;
 let cons = {};
 let paths = [];
 let numPath = 0;
+let memo = {};
+
+function solve(src,dest) {
+	let hash = src+dest;
+	if(memo[hash]) {
+		return memo[hash];
+	}
+	if(src===dest) {
+		memo[hash] = 1;
+		return 1;
+	}
+
+	let ret = 0;
+	let nexts = cons[src];
+	for(let i=0;i<nexts.length;i++) {
+		ret += solve(nexts[i],dest);
+	}
+	memo[hash] = ret;
+	return ret;
+}
 
 eachLine(filename, function(line) {
 	let parts = line.split(": ");
@@ -19,28 +39,34 @@ eachLine(filename, function(line) {
 		cons[parts[0]].push(nexts[i]);
 	}
 }).then(function(err) {
-	let v = {};
-	v["svr"] = 1;
-	paths.push(['svr',v]);
-	while(paths.length>0) {
-		let can = paths.shift();
-		let nexts = cons[can[0]];
-		for(let i=0;i<nexts.length;i++) {
-			if(nexts[i]==="out") {
-				if(can[1]["fft"]&&can[1]["dac"]) {
-					numPath++;
-				}
-			} else {
-				if(can[1][nexts[i]]) {
-					console.log(`looped,skipping`);
-				} else {
-					let visit = Object.assign({}, can[1]);
-					visit[nexts[i]]=1;
-					paths.push([nexts[i],visit]);
-				}
-			}
-		}
-	}
-	answer = numPath;
+	let numSvrDac = 0;
+	let numSvrFft = 0;
+	let numDacFft = 0;
+	let numFftDac = 0;
+	let numFftOut = 0;
+	let numDacOut = 0;
+	cons["out"] = [];
+	memo = {};
+	numSvrDac = solve('svr','dac');
+	console.log(`numSvrDac = ${numSvrDac}`);
+	memo = {};
+	numSvrFft = solve('svr','fft');
+	console.log(`numSvrFft = ${numSvrFft}`);
+	memo = {};
+	numDacFft = solve('dac','fft');
+	console.log(`numDacFft = ${numDacFft}`);
+	memo = {};
+	numFftDac = solve('fft','dac');
+	console.log(`numFftDac = ${numFftDac}`);
+	memo = {};
+	numFftOut = solve('fft','out');
+	console.log(`numFftOut = ${numFftOut}`);
+	memo = {};
+	numDacOut = solve('dac','out');
+	console.log(`numDacOut = ${numDacOut}`);
+	let dacFirst = numSvrDac * numDacFft * numFftOut;
+	let fftFirst = numSvrFft * numFftDac * numDacOut;
+	console.log(`dacFirst = ${dacFirst} and fftFirst = ${fftFirst}`);
+	answer = dacFirst + fftFirst;
   console.log(answer);
 });
